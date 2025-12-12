c8a585646b7d405ddcb55c15bc9797e3e630889c
kieran091 <2231764545@qq.com>
2025-12-12 20:20:32 +0800

    feat: add the IP field to the log

--- log.go
+++ log.go
@@ -1,11 +1,64 @@
 package pilot
 
 import (
+	"net"
+	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 )
+
+func getClientIP(r *http.Request) string {
+	// X-Forwarded-For 可能包含多个IP，取第一个
+	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
+		ips := strings.Split(xff, ",")
+		if len(ips) > 0 {
+			ip := strings.TrimSpace(ips[0])
+			if net.ParseIP(ip) != nil {
+				return ip
+			}
+		}
+	}
+
+	// X-Real-IP
+	if xri := r.Header.Get("X-Real-IP"); xri != "" {
+		if net.ParseIP(xri) != nil {
+			return xri
+		}
+	}
+
+	// X-Client-IP
+	if xci := r.Header.Get("X-Client-IP"); xci != "" {
+		if net.ParseIP(xci) != nil {
+			return xci
+		}
+	}
+
+	// CF-Connecting-IP (Cloudflare)
+	if cfip := r.Header.Get("CF-Connecting-IP"); cfip != "" {
+		if net.ParseIP(cfip) != nil {
+			return cfip
+		}
+	}
+
+	// True-Client-IP
+	if tci := r.Header.Get("True-Client-IP"); tci != "" {
+		if net.ParseIP(tci) != nil {
+			return tci
+		}
+	}
+
+	// RemoteAddr
+	if ip, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+		if net.ParseIP(ip) != nil {
+			return ip
+		}
+	}
+
+	return r.RemoteAddr
+}
 
 func formatDuration(d time.Duration) string {
 	us := d.Microseconds()
@@ -26,7 +79,7 @@ func formatDuration(d time.Duration) string {
 func buildLogEvent(c *Context, isError bool) *zerolog.Event {
 	duration := c.Duration()
 	status := c.Writer.Status()
-	clientIP := ""
+	clientIP := getClientIP(c.Request)
 
 	traceID, _ := c.Get("trace_id")
 	service := c.GetService()
